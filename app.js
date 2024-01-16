5b2a2696e60f3e281a5a3010bd20b573863b6081
ben sinvani <bensinvani7@gmail.com>
2024-01-16 19:54:54 +0200

    save 1

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express'); // קישור לספריית אקספרס
 const app = express(); // יצירת יישום, מימוש של הספריה
 const productRouter = require('./api/v1/routes/product');
 const CategoryRouter = require('./api/v1/routes/category');
+const morgan = require('morgan'); // קישור לספרייה של מורגן 
+app.use(morgan('dev')); // הוספת שכבה שמבצעת לוג לכל הבקשות שהתקבלו 
+
 
 app.use(express.json()); // הוספת שכבה שמטפלת בבקשות שכוללות בודי בקידוד ג'ייסון
 app.use(express.urlencoded()); // הוספת שכבה שמטפלת בבקשות שכוללות בודי בקידוד urluncoded
