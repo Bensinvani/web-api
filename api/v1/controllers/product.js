f47b28f55beb84ecbc58292c8a6c55b80052c999
ben sinvani <bensinvani7@gmail.com>
2024-01-16 19:41:40 +0200

    save 1

--- api/v1/controllers/product.js
+++ api/v1/controllers/product.js
@@ -9,13 +9,13 @@ module.exports = {
         return res.status(200).json({msg:`Get Product id ${pid}`});
     },
     AddProduct:(req,res) =>{
-        const prod = req.body;
+        const body = req.body;
         return res.status(200).json({msg:"Added New Product", body});
     },
     UpdateProduct: (req,res)=>{ // נקודת קצה עבור עדכון מוצר לפי מזהה מוצר
-        
+        let id =req.params.id;
         let pid = req.params.id; // שמירת מזהה המוצר שנשלח
-        return res.status(200).json({msg:`Uptade Product id ${pid}`});
+        return res.status(200).json({msg:`Uptade Product ${id}` , body});
     },
     DeleteProduct:  (req,res)=>{ // נקודת קצה עבור מחיקת מוצר לפי מזהה מוצר
         
--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express'); // קישור לספריית אקספרס
 const app = express(); // יצירת יישום, מימוש של הספריה
 const productRouter = require('./api/v1/routes/product');
 const CategoryRouter = require('./api/v1/routes/category');
+
+app.use(express.json()); // הוספת שכבה שמטפלת בבקשות שכוללות בודי בקידוד ג'ייסון
+app.use(express.urlencoded()); // הוספת שכבה שמטפלת בבקשות שכוללות בודי בקידוד urluncoded
+
 app.use('/product',productRouter); // קישור הראוטר אל האפליקציה
 app.use('/category',CategoryRouter); // קישור הראוטר של קטגוריות
 app.all('*',(req,res)=>{ // הגדרת נקודת קצה עבור כתובת לא קיימת שגיאה 404
