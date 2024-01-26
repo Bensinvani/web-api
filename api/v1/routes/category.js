502ca5efdd71e7d6c9a934c32749854a04fb545d
ben sinvani <bensinvani7@gmail.com>
2024-01-26 12:15:35 +0200

    save 1

--- api/v1/routes/category.js
+++ api/v1/routes/category.js
@@ -7,7 +7,6 @@ const {
     GetCategoryById,
     Addcategory,
     UpdateCategory,
-    RemoveCategoryById,   
     RemoveCategoryById
 } = require('../controllers/category');
 
--- api/v1/routes/product.js
+++ api/v1/routes/product.js
@@ -1,13 +1,13 @@
 // נגדיר ראוטר דרכו נגדיר את כל נקודות הקצה
 const express = require('express');
 const router =  express.Router();
-//const productController = require('../controllers/product');
+// const productController = require('../controllers/product');
 const {GetAllProducts, // ייבוא סלקטיבי של הפונקציות המבוקשות
     GetProductById,
     AddProduct,
     UpdateProduct,
-    RemoveProductById,
-    RemoveProductById} = require('../controllers/product');
+    RemoveProductById
+    } = require('../controllers/product');
 router.get('/',GetAllProducts);
 router.post('/',AddProduct);
 router.get('/:id',GetProductById);
