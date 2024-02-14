3b5f8d50af92c493c36472833e3d33461af297c1
ben sinvani <bensinvani7@gmail.com>
2024-02-14 14:03:11 +0200

    SAVE 1

--- api/v1/routes/product.js
+++ api/v1/routes/product.js
@@ -1,6 +1,7 @@
 // נגדיר ראוטר דרכו נגדיר את כל נקודות הקצה
 const express = require('express');
 const router =  express.Router();
+const auth = require('../../../auth');
 // const productController = require('../controllers/product');
 const {GetAllProducts, // ייבוא סלקטיבי של הפונקציות המבוקשות
     GetProductById,
@@ -9,9 +10,9 @@ const {GetAllProducts, // ייבוא סלקטיבי של הפונקציות המ
     RemoveProductById
     } = require('../controllers/product');
 router.get('/',GetAllProducts);
-router.post('/',AddProduct);
+router.post('/',auth,AddProduct);
 router.get('/:id',GetProductById);
-router.patch('/:id',UpdateProduct);
-router.delete('/:id',RemoveProductById);
+router.patch('/:id',auth,UpdateProduct);
+router.delete('/:id',auth,RemoveProductById);
 
 module.exports = router; // ייצוא של הראוטר
